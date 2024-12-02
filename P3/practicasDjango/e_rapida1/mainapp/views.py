b5385ad1606f89f6ddecd70ba934683c3370d3bf
DiegoH1875 <130120455+DiegoH1875@users.noreply.github.com>
2024-12-02 00:08:24 -0600

    COMMIT P3 DJANGO V2

---
+++ P3/practicasDjango/e_rapida1/mainapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'mainapp/index.html', {
+        'title':'Inicio | Página principal',
+        'content':'Bienvenido a mi ágina principal'
+    })
+
+def about(request):
+    return render(request, 'mainapp/about.html', {
+        'title':'Inicio | Acerca de',
+        'content':'Somos un equipo de desarrollo con Django'        
+    })
+
+def mision(request):
+    return render(request, 'mainapp/mision.html', {
+        'title':'Misión',
+        'content':'Somos un equipo de desarrollo con Django'        
+    })
+
+def vision(request):
+    return render(request, 'mainapp/vision.html', {
+        'title':'Visión',
+        'content':'Somos un equipo de desarrollo con Django'        
+    })
